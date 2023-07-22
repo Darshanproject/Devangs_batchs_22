8f7ef1f9410bee0d6ecfd097aef1ff5a061ddb9e
Darshanproject <gadadarshan6184@gmail.com>
2023-07-22 17:23:21 +0530

    Done

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int       num=10;         //d,f,lf,c,s
+	float  num1=10.25;
+	double  num2=10.358;
+	char name[20]="Devang";
+	
+	
+	printf("%d \n",num);
+	printf("%f \n",num1);
+	printf("%lf \n",num2);
+	printf("%s \n",name);	
+}
