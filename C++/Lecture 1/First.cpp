15a261cd4e7c065d0b6ed2b594bce51552a5b4b8
Darshanproject <gadadarshan6184@gmail.com>
2023-08-31 18:39:09 +0530

    Done

---
+++ C++/Lecture 1/First.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	char name[25];
+	cout<<"Enter your  name : "<<endl;
+	cin>>name;
+	cout<<"Enter your age : "<<endl;
+	cin>>age;
+	cout<<"This is your name and age "<<name<<age;
+	return 0;
+}
